ee6e992bc7029e171e99904ca4aa02615a807ed1
Merv39 <78385901+Merv39@users.noreply.github.com>
2024-11-13 13:03:44 +0000

    tutorial: react tutorial follow-along
    Following https://www.youtube.com/watch?v=SqcY0GlETPk to get practice and re-learn web dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><ListGroup/></div>
+  let items = ["Tokyo", "London", "Shanghai", "Singapore", "Dublin"];
+
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={(item) => console.log(item)}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,7 +1,17 @@
-function ListGroup() {
-  let items = ["Tokyo", "London", "Shanghai", "Singapore", "Dublin"];
+import { useState } from "react";
 
-  items = [];
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  // Hook Function
+  const arr = useState(-1);
+  // arr[0] // variable
+  // arr[1] // setter function
+  const [selectedIndex, setSelectedIndex] = arr;
 
   const getMessage = () => {
     return items.length === 0 && <p>No item found</p>;
@@ -9,11 +19,24 @@ function ListGroup() {
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {getMessage()}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
