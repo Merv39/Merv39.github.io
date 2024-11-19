9e53eb6265aa1b3bd1e92e976f4b1411406dd15f
Merv39 <78385901+Merv39@users.noreply.github.com>
2024-11-19 17:25:00 +0000

    tutorial: react tutorial follow along 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
   let items = ["Tokyo", "London", "Shanghai", "Singapore", "Dublin"];
 
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   // const handleSelectItem = (item: string) => {
   //   console.log(item);
   // };
 
+  /*      
+  <ListGroup
+    items={items}
+    heading="Cities"
+    onSelectItem={(item) => console.log(item)}
+  />
+  */
+
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={(item) => console.log(item)}
-      />
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
     </div>
   );
 }
